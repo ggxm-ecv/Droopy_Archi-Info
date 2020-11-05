9109c060986a2b08a98f7aacc357e45694d88069
Gautier Maire <gautier.maire@dotsafe.fr>
2020-11-05 16:01:35 +0100

    add my geolacation + layer + marquer + style + font

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ if ("geolocation" in navigator) {
   console.log('OK geolocation')
   navigator.geolocation.getCurrentPosition(function(position) {
     console.log(position.coords);
+
+    // Afficher la carte
+    var myMap = L.map('myMap').setView([position.coords.latitude, position.coords.longitude], 20);
+    console.log(myMap);
+
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png').addTo(myMap);
+
+    var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(myMap);
+
+    // Un autre Layer
+    /* https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png */
   });
 
 
