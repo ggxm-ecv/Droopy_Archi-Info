e597a38a60b3f0902c46f82e119e43a4e2eb18c4
Gautier Maire <gautier.maire@dotsafe.fr>
2020-11-05 19:44:51 +0100

    add research for the end point but fail :boom:

--- js/main.js
+++ js/main.js
@@ -30,9 +30,9 @@
 //   })
 // }
 
-/*----------------------------------------------
+/*--------------------------------------------------------------------
 // New - Fonction pour récupérer les arbres : with my position
-//----------------------------------------------*/
+//--------------------------------------------------------------------*/
 function getTrees(lat, lng) {
   return new Promise( function(resolve, reject){
     fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-arbres&q=&rows=5000&facet=domanialite&facet=arrondissement&facet=libellefrancais&facet=circonferenceencm&facet=hauteurenm&facet=remarquable&geofilter.distance='+lat+'%2C+'+lng+'%2C+1000')
@@ -88,9 +88,9 @@ if ("geolocation" in navigator) {
     //     console.error(error);
     //   })
 
-    /*------------------------------------
+    /*----------------------------------------------------------
     // New - Charger la liste des arbres : with my position
-    //------------------------------------*/
+    //----------------------------------------------------------*/
     getTrees(position.coords.latitude, position.coords.longitude)
       .then(function(data){
         console.log(data.records);
@@ -111,3 +111,124 @@ if ("geolocation" in navigator) {
   /* la géolocalisation n'est pas disponible */
   console.log('NOT geolocation')
 }
+
+//--------------------------------------------
+// Fonction pour vérifier si un point est dans un polygone
+//--------------------------------------------
+function isMarkerInsidePolygon(marker, poly) {
+  var polyPoints = poly.getLatLngs();
+  var x = marker.getLatLng().lat, y = marker.getLatLng().lng;
+
+  var inside = false;
+  for (var i = 0, j = polyPoints.length - 1; i < polyPoints.length; j = i++) {
+    var xi = polyPoints[i].lat, yi = polyPoints[i].lng;
+    var xj = polyPoints[j].lat, yj = polyPoints[j].lng;
+
+    var intersect = ((yi > y) != (yj > y))
+        && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+    if (intersect) inside = !inside;
+  }
+
+  return inside;
+};
+
+//--------------------------------------------
+// Pour avoir des zones avec voronoi diagram (avec la data au format geojson)
+//--------------------------------------------
+
+// d3.json('point.geojson', function(geojson){
+//   mapDraw(geojson);
+// });
+//
+//
+// function mapDraw(geojson){
+//
+//   var pointdata = geojson.features;
+//
+//
+//
+// 	var map = L.map('map');
+// 	map.setView([36.3894816, 139.0634281], 14);
+// 	map.on("viewreset moveend", update);
+//
+// 	var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+//
+// 	L.tileLayer(
+// 		'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+// 		{
+// 			attribution: 'Map data &copy; ' + mapLink,
+// 			maxZoom: 18
+// 		}
+// 	).addTo(map);
+//
+//
+//
+// 	map._initPathRoot();
+// 	var svg = d3.select("#map").select("svg");
+// 	var g = svg.append("g").attr("class", "leaflet-zoom-hide");
+//
+//
+//
+// 	var voronoi = d3.geom.voronoi()
+// 		.x(function(d) { return d.x; })
+// 		.y(function(d) { return d.y; });
+//
+//
+//
+//    update();
+//
+//
+// 	function update() {
+//
+//
+// 		var positions = [];
+//
+//
+// 		pointdata.forEach(function(d) {
+// 			var latlng = new L.LatLng(d.geometry.coordinates[1], d.geometry.coordinates[0]);
+// 			positions.push({
+// 				x :map.latLngToLayerPoint(latlng).x,
+// 				y :map.latLngToLayerPoint(latlng).y
+// 			});
+// 		});
+//
+//
+//
+// 		d3.selectAll('.AEDpoint').remove();
+//
+// 		var circle = g.selectAll("circle")
+// 			.data(positions)
+// 			.enter()
+// 			.append("circle")
+// 			.attr("class", "AEDpoint")
+// 			.attr({
+// 				"cx":function(d, i) { return d.x; },
+// 				"cy":function(d, i) { return d.y; },
+// 				"r":2,
+// 				fill:"red"
+// 			});
+//
+//
+// 		var polygons = voronoi(positions);
+// 		polygons.forEach(function(v) { v.cell = v; });
+//
+//
+// 		svg.selectAll(".volonoi").remove();
+//
+// 		svg.selectAll("path")
+// 			.data(polygons)
+// 			.enter()
+// 			.append("svg:path")
+// 			.attr("class", "volonoi")
+// 			.attr({
+// 				"d": function(d) {
+// 				if(!d) return null;
+// 					return "M" + d.cell.join("L") + "Z";
+// 				},
+// 				stroke:"black",
+// 				fill:"none"
+// 			});
+//
+//     }
+//
+// }
